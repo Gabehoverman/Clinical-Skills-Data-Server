1d8abe534263e787678e2773010a2526d6d36294
Nick Alexander <alexander50@live.com>
2015-12-28 03:04:27 -0500

    Add very basic template files

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -2,7 +2,7 @@ class DataController < ApplicationController
   def systems
     @systems = System.where(parent: nil)
     respond_to do |format|
-      format.html { render :base }
+      format.html { render :systems }
       format.json { render json: @systems, status: :ok }
     end
   end
@@ -10,16 +10,8 @@ class DataController < ApplicationController
   def subsystems
     @systems = System.where.not(parent: nil)
     respond_to do |format|
-      format.html { render :base }
+      format.html { render :subsystems }
       format.json { render json: @systems, status: :ok }
-    end
-  end
-
-  def links
-    @links = Link.all
-    respond_to do |format|
-      format.html { render :all }
-      format.json { render json: @links, status: :ok }
     end
   end
 
@@ -28,6 +20,14 @@ class DataController < ApplicationController
     respond_to do |format|
       format.html { render :detailed }
       format.json { render json: @systems, status: :ok }
+    end
+  end
+
+  def links
+    @links = Link.all
+    respond_to do |format|
+      format.html { render :links }
+      format.json { render json: @links, status: :ok }
     end
   end
 end
