0dd85cbab5d548ff567d7e8339a66162a4ef1d98
Nick Alexander <alexander50@marshall.edu>
2016-04-30 23:49:32 -0700

    Working version of Video Links table. Precompile Assets

---
+++ app/assets/dialog_controllers/video_links/video_link_edit_special_tests_dialog_controller.js
@@ -0,0 +1,75 @@
+function EditSpecialTestsController($scope, $mdDialog, videoLink, allSpecialTests, editing) {
+
+    $scope.videoLink = videoLink;
+    $scope.allSpecialTests = allSpecialTests;
+    $scope.usedSpecialTests = [];
+    $scope.unusedSpecialTests = [];
+    $scope.editing = editing;
+
+    $scope.isInitialFilter = true;
+
+    $scope.query = {
+        order: 'name',
+        limit: 10,
+        page: 1,
+        filter: ""
+    };
+
+    $scope.filterSpecialTests = function () {
+        for (var i = 0; i < $scope.allSpecialTests.length; i++) {
+            var specialTest = $scope.allSpecialTests[i];
+            if (($scope.isInitialFilter) ? indexOfItemWithID(specialTest.id, $scope.videoLink.special_tests) == -1 : indexOfItemWithID(specialTest.id, $scope.usedSpecialTests) == -1) {
+                if (indexOfItemWithID(specialTest.id, $scope.unusedSpecialTests) == -1) {
+                    if (!specialTest.videoLink) {
+                        $scope.unusedSpecialTests.push(specialTest);
+                    }
+                }
+            } else {
+                if (indexOfItemWithID(specialTest.id, $scope.usedSpecialTests) == -1) {
+                    $scope.usedSpecialTests.push(specialTest);
+                }
+            }
+        }
+        $scope.isInitialFilter = false;
+    };
+
+    $scope.remove = function(specialTestToRemove) {
+        var index = indexOfItemWithID(specialTestToRemove.id, $scope.usedSpecialTests);
+        if (index != -1) {
+            var allIndex = indexOfItemWithID(specialTestToRemove.id, $scope.allSpecialTests);
+            if (allIndex != -1) {
+                $scope.allSpecialTests[allIndex].videoLink = null;
+            }
+            $scope.usedSpecialTests.splice(index, 1);
+            $scope.filterSpecialTests();
+        }
+    };
+
+    $scope.addSpecialTest = function () {
+        if($scope.specialTestToAdd) {
+            $scope.usedSpecialTests.push($scope.specialTestToAdd);
+            var index = indexOfItemWithID($scope.specialTestToAdd.id, $scope.unusedSpecialTests);
+            $scope.unusedSpecialTests.splice(index, 1);
+            var allIndex = indexOfItemWithID($scope.specialTestToAdd.id, $scope.allSpecialTests);
+            if (allIndex != -1) {
+                $scope.allSpecialTests[allIndex].videoLink = { "name": $scope.videoLink.name };
+            }
+            $scope.specialTestToAdd = null;
+            $scope.filterSpecialTests();
+        }
+    };
+
+    $scope.save = function() {
+        $mdDialog.hide($scope.usedSpecialTests);
+    };
+
+    $scope.cancel = function() {
+        $mdDialog.hide(null);
+    };
+
+    $scope.hide = function() {
+        $mdDialog.hide(null);
+    };
+
+    $scope.filterSpecialTests();
+}
--- app/controllers/video_links_controller.rb
+++ app/controllers/video_links_controller.rb
@@ -2,6 +2,9 @@ class VideoLinksController < ApplicationController
 
   def index
     @toolbar_title = 'Video Links'
+    js :new_video_link_dialog_template_url => ActionController::Base.helpers.asset_path('new_video_link_dialog.html')
+    js :edit_exam_techniques_dialog_template_url => ActionController::Base.helpers.asset_path('video_link_edit_exam_techniques_dialog.html')
+    js :edit_special_tests_dialog_template_url => ActionController::Base.helpers.asset_path('video_link_edit_special_tests_dialog.html')
     respond_to do |format|
       if not params['special_test'].nil?
         format.json { render json: VideoLink.api_for_special_test_name(params['special_test']), status: :ok }
@@ -13,5 +16,62 @@ class VideoLinksController < ApplicationController
       format.html { render :index, status: :ok }
     end
   end
+
+  def create
+    @video_link = VideoLink.new(video_link_params)
+    respond_to do |format|
+      if @video_link.save
+        format.json { render json: @video_link.as_json(root: true), status: :ok }
+      else
+        format.json { render json: @video_link.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    @video_link = VideoLink.find(params[:id])
+    @video_link.exam_techniques.clear
+    unless params['exam_techniques'].nil?
+      params['exam_techniques'].each do |e|
+        exam_technique = ExamTechnique.find(e['id'])
+        unless @video_link.exam_techniques.include?(exam_technique)
+          @video_link.exam_techniques << exam_technique
+        end
+      end
+    end
+    @video_link.special_tests.clear
+    unless params['special_tests'].nil?
+      params['special_tests'].each do |e|
+        special_test = SpecialTest.find(e['id'])
+        unless @video_link.special_tests.include?(special_test)
+          @video_link.special_tests << special_test
+        end
+      end
+    end
+    respond_to do |format|
+      if @video_link.update(video_link_params)
+        format.json { render json: @video_link, status: :ok }
+      else
+        format.json { render json: @video_link.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @video_link = VideoLink.find(params[:id])
+    respond_to do |format|
+      if @video_link.delete
+        format.json { render json: @video_link, status: :ok }
+      else
+        format.json { render json: @video_link.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+    def video_link_params
+      params.permit(:id, :title, :link)
+    end
 
 end
