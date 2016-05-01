2718e29d573bb7c3f7f5f0653a684555a21ded1b
Nick Alexander <alexander50@live.com>
2016-04-30 17:44:19 -0700

    Precompile assets

---
+++ public/assets/palpations/new_palpation_dialog_controller-c9d8a6e03c9e72b59080c086679d999a85224bd6124e389767ef92af8e61aa5e.js
@@ -0,0 +1,28 @@
+function NewPalpationDialogController($scope, $mdDialog, allComponents) {
+
+    $scope.allComponents = allComponents;
+
+    $scope.newPalpation = {
+        structure: "",
+        component: {
+            id: -1,
+            name: ""
+        },
+        details: "",
+        notes: ""
+    };
+
+    $scope.close = function() {
+        $mdDialog.cancel();
+    };
+
+    $scope.create = function() {
+        if ($scope.newPalpation.structure === "" || $scope.selectedComponent.name !== "") {
+            $scope.newPalpationForm.$setSubmitted();
+        } else {
+            $scope.newPalpation.component.id = $scope.selectedComponent.id;
+            $scope.newPalpation.component.name = $scope.selectedComponent.name;
+            $mdDialog.hide($scope.newPalpation);
+        }
+    };
+}
